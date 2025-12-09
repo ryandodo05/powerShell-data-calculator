d6e049840f8f1dd5fc07ae252f712c18dbe0e0c9
dodo_ryan <ryan.kraus.pro@gmail.com>
2025-12-09 16:18:05 +0100

    first commit

---
+++ drives.ps1
@@ -0,0 +1,12 @@
+﻿$drives = Get-PSDrive -PSProvider FileSystem | ForEach-Object {
+    $used = $_.Used
+    $free = $_.Free
+    $total = $used + $free
+    [PSCustomObject]@{
+        Name  = $_.Name
+        TotalGB = "{0:N2} GB" -f ($total / 1GB)
+        UséeGB  = "{0:N2} GB" -f ($used / 1GB)
+        libreGB  = "{0:N2} GB" -f ($free / 1GB)
+    }
+}
+$drives | Sort-Object UsedGB -Descending | Format-Table
